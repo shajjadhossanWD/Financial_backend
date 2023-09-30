0e7d3c6a8fc2b5e74a63d821ba5b3ced639f8a0b
Mehedi Hassan <rehan51322@gmail.com>
2023-10-01 00:38:01 +0600

    completed upto user mail

---
+++ utils/sendMail.ts
@@ -0,0 +1,41 @@
+require("dotenv").config();
+import nodeMailer, { Transporter } from "nodemailer";
+import ejs from "ejs";
+import path from "path";
+
+interface EmailOption {
+  email: string;
+  subject: string;
+  template: string;
+  data: { [key: string]: any };
+}
+
+const sendMail = async (options: EmailOption) => {
+  const transporter: Transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+  const { email, subject, template, data } = options;
+
+  // get the path to the email template file
+  const templatePath = path.join(__dirname, "../mails", template);
+
+  // render the email template
+  const html: string = await ejs.renderFile(templatePath, data);
+
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject,
+    html,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export default sendMail;
