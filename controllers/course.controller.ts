ba6fbc0edbb414664689b8fb3ba3c493377322e5
Mehedi Hassan <rehan51322@gmail.com>
2023-10-06 01:46:18 +0600

    get course content by user done

--- controllers/course.controller.ts
+++ controllers/course.controller.ts
@@ -133,3 +133,35 @@ export const getAllCourses = CatchAsyncError(
     }
   }
 );
+
+// get course content -- only for valid users
+
+export const getCourseByUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+      const courseId = req.params.id;
+
+      const courseExist = userCourseList?.find(
+        (course: any) => course._id.toString() === courseId
+      );
+
+      if (!courseExist) {
+        return next(
+          new ErrorHandler("You are not eligible to access this course", 404)
+        );
+      }
+
+      const course = await CourseModel.findById(courseId);
+
+      const content = course?.courseData;
+
+      res.status(200).json({
+        success: true,
+        content,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
